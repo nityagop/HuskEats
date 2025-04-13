46954d6b1df95f4156fac7bd835f30b3c698aa1c
glor-n <ge.gl@northeastern.edu>
2025-04-13 15:58:23 -0400

    Update 10_Student_Home.py

--- app/src/pages/10_Student_Home.py
+++ app/src/pages/10_Student_Home.py
@@ -6,25 +6,18 @@ from modules.nav import SideBarLinks
 
 st.set_page_config(layout = 'wide')
 
-# Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome USAID Worker, {st.session_state['first_name']}.")
-st.write('')
-st.write('')
+
+st.title(f"Welcome {st.session_state['first_name']}.")
 st.write('### What would you like to do today?')
 
-if st.button('Predict Value Based on Regression Model', 
+if st.button('View my Favorites', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/11_Prediction.py')
+  st.switch_page('pages/student_favorites')
 
-if st.button('View the Simple API Demo', 
+if st.button('View Resturaunts', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/12_API_Test.py')
-
-if st.button("View Classification Demo",
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/13_Classification.py')
+  st.switch_page('pages/')
