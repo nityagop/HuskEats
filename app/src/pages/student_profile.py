73041401daec7d9d28c0caae04739ebf8015c703
glor-n <ge.gl@northeastern.edu>
2025-04-13 16:33:18 -0400

    student routes

--- app/src/pages/student_profile.py
+++ app/src/pages/student_profile.py
@@ -15,14 +15,10 @@ st.write('### What would you like to do today?')
 if st.button('View my Favorites', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/student_favorites.py')
+  st.switch_page('pages/student_favorites')
+
 
 if st.button('View Resturaunts', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/11_View_Resteraunt.py')
-
-if st.button('Write Reviews', 
              type='primary',
              use_container_width=True):
   st.switch_page('pages/')
