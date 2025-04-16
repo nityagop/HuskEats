20948e914b5f87c477b02bff7c6394e74b658748
Aura Herce-Duchini <129234288+auraHerce@users.noreply.github.com>
2025-04-16 18:54:14 -0400

    Show student favorites for mock user

--- app/src/pages/13_Student_Favorites.py
+++ app/src/pages/13_Student_Favorites.py
@@ -13,6 +13,8 @@ import requests
 # Call the SideBarLinks from the nav module in the modules directory
 SideBarLinks()
 user_id = st.session_state.get('user_id')
+user_id = 1 # for testing purposes
+
 # set the header of the page
 st.header('Favorites')
 
@@ -27,31 +29,17 @@ else:
 
     event = st.dataframe(
         st.session_state.df,
+        column_order = [
+            "Restaurant Name",
+            "Address",
+            "Rating",
+            "Restaurant Description",
+        ],
         on_select="rerun",
         selection_mode="single-row",
     )
 
     selected_rows = event.selection["rows"]
-
-# if selected_rows:
-#     selected_index = selected_rows[0]
-#     selected_restaurant = response[selected_index]
-
-#     st.session_state.restaurant_name = selected_restaurant['Restaurant Name']
-#     st.session_state.restaurant_id = selected_restaurant.get('Restaurant ID')
-
-#     st.divider()
-#     st.write("### Restaurant Details")
-#     st.write(f"**Name:** {selected_restaurant['Restaurant Name']}")
-#     st.write(f"**Address:** {selected_restaurant['Address']}")
-#     st.write(f"**Description:** {selected_restaurant['Restaurant Description']}")
-
-#     left, right = st.columns(2)
-#     if left.button("View Restaurant Profile", use_container_width=True):
-#         st.write("Going to restaurant profile...")
-
-#     if right.button("Write Review", type="primary", use_container_width=True):
-#         st.switch_page('pages/reviewforrest.py')
 
 
 
